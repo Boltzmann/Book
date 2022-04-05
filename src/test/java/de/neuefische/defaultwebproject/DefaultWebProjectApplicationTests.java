e52b139f8932665dd602fe06c0a44e8b77b429e8
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-05 16:40:02 +0200

    Initial commit

---
+++ src/test/java/de/neuefische/defaultwebproject/DefaultWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.defaultwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
