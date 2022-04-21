5d821b683c3df6195028c94882065e591a71d40d
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-21 17:11:27 +0200

    Nice pink layout and other niceties.

--- frontend/src/component/BookCard.tsx
+++ frontend/src/component/BookCard.tsx
@@ -1,11 +1,12 @@
 import {Book} from "../model/Book";
+import "./BookCard.css"
 
 type BookProps = {
     book : Book;
 }
 
 export default function BookCard({book} : BookProps){
-    return <div>
+    return <div id="card">
         <div className="bookText">{book.isbn}</div>
         <div className="bookText" >{book.name}</div>
     </div>
--- frontend/src/component/BookList.tsx
+++ frontend/src/component/BookList.tsx
@@ -1,5 +1,6 @@
 import {Book} from "../model/Book";
 import BookCard from "./BookCard";
+import "./BookList.css"
 
 type BookListProps = {
     books : Book[]
@@ -9,7 +10,7 @@ export default function BookList({books}:BookListProps){
     return <div id="bookList">
         <div>
             {books.length > 0 ?
-                (<div id="gallery-cards"> {books.map(book => <BookCard book={book}/>)} </div>) :
+                (<div id="book-cards"> {books.map(book => <BookCard book={book}/>)} </div>) :
                 (<div>No Book</div>)
             }
         </div></div>
